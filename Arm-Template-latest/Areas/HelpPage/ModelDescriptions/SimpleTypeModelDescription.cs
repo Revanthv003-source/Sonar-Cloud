ea8beb1f4d64926867317d93b097ed349a19bddb
revanth <revanthv73@gmail.com>
2021-07-20 04:42:09 +0530

    3rd commit

--- Arm-Template-latest/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Arm_Template_latest.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Arm-Template-latest/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Arm_Template_latest.WebApiApplication" Language="C#" %>
